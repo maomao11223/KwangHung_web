db17a9ef002872148830e5816309ef40e3e5fe3f
maomao11223 <skyhigh5579@gmail.com>
2021-07-22 19:45:19 +0800

    完成header,nav

---
+++ js/script_header nav_0722.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+// 導覽列開關 開始
+    let $menu = $(".nav-menu"),
+        $sideNav = $(".nav"),
+        $btncancel = $("#btnCancel");
+    
+    $($menu).click(function(){
+        $sideNav.toggleClass("active");
+    });
+
+    $($btncancel).click(function(){
+        $sideNav.removeClass("active");
+    });
+    $('.sidenav > li').click(function(){
+        $sideNav.removeClass("active");
+    });
+
+    //  品牌手風琴 開始
+    $('.sideBrand > li > ul').hide();
+    $('.searchLanguage > li > ul').hide();
+
+
+    $('.sideBrand > li > ul').click(function (event) {
+        event.stopPropagation();
+    });
+    $('.searchLanguage > li > ul').click(function (event) {
+        event.stopPropagation();
+    });
+
+    $('.sideBrand > li').click(function () {
+        $(this).find('ul').slideToggle().parent().siblings().find('ul').slideUp();
+    });
+    $('.searchLanguage > li').click(function () {
+        $(this).find('ul').slideToggle().parent().siblings().find('ul').slideUp();
+    });
+
+
+    document.addEventListener("click",clickHidden);
+    // 品牌手風琴 結束
+
+});
